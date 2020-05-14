54797b5cf61a2e41ddc1cc4884895e2e6fe753fd
Chris Allen <chrisallen415@gmail.com>
2020-05-14 18:39:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -10,8 +12,6 @@ end
 def input_to_index(input)
   index = input.to_i - 1
 end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
 
 def move(board, index, player = 'X')
   board[index] = player
